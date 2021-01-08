1b1248d1ece276eeebd9aa96966615a21c9b7c33
Luis Zimmermann <luisaslan@gmail.com>
2021-01-08 19:04:14 -0300

    Alterando animações dos personagens, corrigindo bug no seletor de levels e aplicando parallax nos bgs.
    Alterando animações dos personagens, corrigindo bug no seletor de levels e aplicando parallax nos bgs.

---
+++ [Unity] Financinha/Assets/Scripts/Gameplay/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Renderer bgMaterial;
+    public float parallaxSpeed;
+
+    private void FixedUpdate()
+    {
+        if (parallaxSpeed < 1.1)
+        {
+            parallaxSpeed += Time.deltaTime / 110;
+        }
+
+        bgMaterial.material.mainTextureOffset += new Vector2(0, parallaxSpeed * Time.deltaTime);
+    }
+}
