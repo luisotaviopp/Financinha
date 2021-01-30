290c17debe93a0c6d23dd308c6be1580a03fcc83
Luis Zimermann <luis.zimermann@equilibriumlogistica.com.br>
2021-01-30 19:14:32 -0300

    DELETANDO DRAGONBONES
    DELETANDO DRAGONBONES

--- [Unity] Financinha/Assets/Scripts/UI/UIManager.cs
+++ [Unity] Financinha/Assets/Scripts/UI/UIManager.cs
@@ -1,16 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public void OpenScene(GameObject panel)
+    public GameObject panelLogin;
+    public GameObject panelRegistration;
+    public GameObject panelWallet;
+    public GameObject panelEconomy;
+    public GameObject panelFamily;
+    public GameObject panelSettings;
+    public GameObject panelRules;
+    public GameObject panelQuizz;
+    public GameObject panelLevelSelector;
+    public GameObject panelPreQuizz;
+
+
+    public void OpenLogin()
     {
-        panel.SetActive(true);
+        panelLogin.SetActive(true);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
     }
 
-    public void CloseScene(GameObject panel)
+    public void OpenRegistration()
     {
-        panel.SetActive(false);
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(true);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenWallet()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(true);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenEconomy()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(true);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenFamily()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(true);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(true);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenRules()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(true);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenQuizz()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(true);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenLevelSelector()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(true);
+        panelPreQuizz.SetActive(false);
+    }
+
+    public void OpenPreQuizz()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelWallet.SetActive(false);
+        panelEconomy.SetActive(false);
+        panelFamily.SetActive(false);
+        panelSettings.SetActive(false);
+        panelRules.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(true);
     }
 }
