29308fc63de499a0331101b278a954e190615250
Jessica Tasso <jessica.tasso@izyplay.com.br>
2021-02-03 06:45:47 -0300

    Ajustes na Game Play E sofrendo no quizz
    Ajustes na Game Play E sofrendo no quizz

---
+++ [Unity] Financinha/Assets/Scripts/Gameplay/Quizz/Quizz.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Quizz : MonoBehaviour
+{
+    public List<GameObject> lala = new List<GameObject>();
+    [System.Serializable]public class Respostas 
+    {
+        public Text alo;
+    }
+
+}
