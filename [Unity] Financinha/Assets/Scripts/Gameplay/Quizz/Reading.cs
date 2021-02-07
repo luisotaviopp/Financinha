a270598d37c6df447497cd3cf9cb2d6d9621eaae
Jessica Tasso <jessica.tasso@izyplay.com.br>
2021-02-07 20:15:47 -0300

    Reading system
    Reading system

---
+++ [Unity] Financinha/Assets/Scripts/Gameplay/Quizz/Reading.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Reading : MonoBehaviour
+{
+    public Image readHistory;
+    public Sprite[] img;
+    public int imgSelect;
+
+    public void Start()
+    {
+        readHistory.sprite = img[imgSelect];
+    }
+    public void Set_NextImg()
+    {
+
+        if (imgSelect < img.Length-1)
+        {
+            imgSelect++;
+            readHistory.sprite = img[imgSelect];
+
+            return;
+        }
+    }
+    public void Set_PreviousImg()
+    {
+        
+        if (imgSelect > 0)
+        {
+            imgSelect--;
+            readHistory.sprite = img[imgSelect];
+            return;
+        }
+    }
+
+}
