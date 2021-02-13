d4e2051c4586fc8cd8e8d7eeecbd7b097433ed48
Luis Zimermann <luis.zimermann@equilibriumlogistica.com.br>
2021-02-13 10:12:01 -0300

    Blocks menu e quiz.
    Blocks menu e quiz.

--- [Unity] Financinha/Assets/Scripts/GameManager.cs
+++ [Unity] Financinha/Assets/Scripts/GameManager.cs
@@ -8,6 +8,12 @@ public class GameManager : MonoBehaviour
 
     public GameObject menuPanel;
     public GameObject loginPanel;
+    public GameObject carteiraPanel;
+    public GameObject presentePanel;
+    public GameObject causasPanel;
+    public GameObject regrasPanel;
+    public GameObject extratosPanel;
+    public GameObject cofrinhoPanel;
 
     public static bool gameIsPlaying = false;
 
@@ -33,12 +39,95 @@ public class GameManager : MonoBehaviour
     {
         menuPanel.SetActive(true);
         loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
     }
+
     public void OpenLogin()
     {
-        loginPanel.SetActive(true);
         menuPanel.SetActive(false);
+        loginPanel.SetActive(true);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
     }
 
+    public void OpenCarteira()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(true);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
+    }
 
+    public void OpenPresente()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(true);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
+    }
+
+    public void OpenCausas()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(true);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
+    }
+
+    public void OpenRegras()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(true);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(false);
+    }
+
+    public void OpenExtratos()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(true);
+        cofrinhoPanel.SetActive(false);
+    }
+
+    public void OpenCofrinho()
+    {
+        menuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        carteiraPanel.SetActive(false);
+        presentePanel.SetActive(false);
+        causasPanel.SetActive(false);
+        regrasPanel.SetActive(false);
+        extratosPanel.SetActive(false);
+        cofrinhoPanel.SetActive(true);
+    }
 }
--- backend-financinha/index.php
+++ backend-financinha/index.php
@@ -11,7 +11,7 @@
 		<a href="api/1.0.0/list_users.php"> VER TODOS OS USUARIOS</a>
 
 		<br><h3>Registrando novo usuário</h3>
-		<form action="api/1.0.0/create_user.php" method="post">
+		<form action="api/1.0.0/create_user.php" method="post" autocomplete="off">
 			<input type="text" name="post_user" placeholder="Usuário">
 			<input type="text" name="post_name"placeholder="Nome">
 			<input type="password" name="post_password"placeholder="Senha">
@@ -21,9 +21,9 @@
 
 
 		<br><h3>Login</h3>
-		<form action="api/1.0.0/login.php" method="post">
+		<form action="api/1.0.0/login.php" method="post" autocomplete="off">
 			<input type="text" name="post_username" placeholder="Usuário">
-			<input type="text" name="post_password" placeholder="Senha">
+			<input type="password" name="post_password" placeholder="Senha">
 			<input type="submit" value="DALE">
 		</form>
 
@@ -36,7 +36,7 @@
 
 
 		<br><h3>Verificando o login de um usuário (se o token é válido)</h3>
-		<form action="api/1.0.0/check_login.php" method="post">
+		<form action="api/1.0.0/check_login.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token" name="post_token" maxlength="128">
 			<input type="submit" value="DALE">
 		</form>
@@ -49,7 +49,7 @@
 
 		
 		<br><h3>Verificando o valor total da carteira</h3>
-		<form action="api/1.0.0/wallet_amount.php" method="post">
+		<form action="api/1.0.0/wallet_amount.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token" name="post_token" maxlength="128">
 			<input type="submit" value="DALE">
 		</form>
@@ -60,7 +60,7 @@
 
 
 		<br><h3>Verificando o valor total do cofrinho</h3>
-		<form action="api/1.0.0/saving_amount.php" method="post">
+		<form action="api/1.0.0/saving_amount.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token" name="post_token" maxlength="128">
 			<input type="submit" value="DALE">
 		</form>
@@ -71,7 +71,7 @@
 
 
 		<br><h3>Verificando o level de um usuário</h3>
-		<form action="api/1.0.0/user_level.php" method="post">
+		<form action="api/1.0.0/user_level.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token" name="post_token" maxlength="128">
 			<input type="submit" value="DALE">
 		</form>
@@ -80,7 +80,7 @@
 
 
 		<br><h3>Aumentando o level de um usuário</h3>
-		<form action="api/1.0.0/increase_level.php" method="post">
+		<form action="api/1.0.0/increase_level.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token" name="post_token" maxlength="128">
 			<input type="submit" value="DALE">
 		</form>
@@ -89,14 +89,14 @@
 
 
 
-		<br><h3>(FAZER) - Listando penalidades de um usuário</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Listando penalidades de um usuário. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Criando regras da casa de um usuário</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Criando regras da casa de um usuário. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="text" placeholder="Nome da regra">
 			<input type="text" placeholder="Descrição">
@@ -104,72 +104,72 @@
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Listando regras da casa de um usuário</h3>
+		<br><h3>Listando regras da casa de um usuário. *</h3>
 		<p>Provavelmente vai precisar de uma tabela de ligação, já que a cardinalidade relacional é N para N.</p>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Transferindo da carteira para o cofrinho</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
-			<input type="text" placeholder="Token">
-			<input type="number" placeholder="Valor">
-			<input type="submit" value="DALE">
-		</form>
-
-		<br><h3>(FAZER) - Transferindo do cofrinho para a carteira</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Transferindo da carteira para o cofrinho. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="number" placeholder="Valor">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Listando os familiáres de um usuário</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Transferindo do cofrinho para a carteira. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
+			<input type="text" placeholder="Token">
+			<input type="number" placeholder="Valor">
+			<input type="submit" value="DALE">
+		</form>
+
+		<br><h3>Listando os familiáres de um usuário. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Procurando um usuário</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Procurando um usuário. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Nome">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Retirando (usando) o dinheiro da carteira.</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Retirando (usando) o dinheiro da carteira. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Motivo">
 			<input type="number" placeholder="Valor">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Extrato.</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>Extrato. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Token">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - (Apenas filhos) - Cadastrando uma resposta para uma pergunta</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>(Apenas filhos) - Cadastrando uma resposta para uma pergunta. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<textarea name="answer" id="" cols="30" rows="10"></textarea>
 			<br><input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - (Apenas pais) - Vendo as respostas para as perguntas.</h3>
-		<form action="api/1.0.0/search_user.php" method="post">
+		<br><h3>(Apenas pais) - Vendo as respostas para as perguntas. *</h3>
+		<form action="api/1.0.0/search_user.php" method="post" autocomplete="off">
 			<input type="text" placeholder="Nome">
 			<input type="submit" value="DALE">
 		</form>
 
-		<br><h3>(FAZER) - Depositando na carteira.</h3>
+		<br><h3>Depositando na carteira. *</h3>
 
-		<br><h3>(FAZER) - Simulação de semanada.</h3>
+		<br><h3>Simulação de semanada. *</h3>
 		<p>valor do objetivo, data para conseguir o objetivo... (tá pronto lá na unity)</p>
 
-		<br><h3>(FAZER) - Cadastrar objetivo.</h3>
+		<br><h3>Cadastrar objetivo. *</h3>
 
-		<br><h3>(FAZER) - Cadastrar causa.</h3>
+		<br><h3>Cadastrar causa. *</h3>
 
 		<br><br><br><br>
 	</body>
