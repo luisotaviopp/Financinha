3961fd2f6135651b686bbb98a1408700b4e702fa
Jessica Tasso <jessica.tasso@izyplay.com.br>
2021-02-26 08:07:22 -0300

    implementando programaçao do menu
    implementando programaçao do menu

--- [Unity] Financinha/Assets/Scripts/UI/UIManager.cs
+++ [Unity] Financinha/Assets/Scripts/UI/UIManager.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject panelLevelSelector;
     public GameObject panelLogin;
     public GameObject panelRegistration;
-    public GameObject panelWallet;
-    public GameObject panelEconomy;
-    public GameObject panelFamily;
-    public GameObject panelSettings;
-    public GameObject panelRules;
     public GameObject panelSavings;
     public GameObject panelPhilantrophy;
     public GameObject panelQuizz;
-    public GameObject panelLevelSelector;
     public GameObject panelPreQuizz;
+    public GameObject panelSettings;
+
+    public GameObject panelBanco_da_Casa;
+    public GameObject panelBanco_da_Casa_Lancar;
+    public GameObject panelCarteira;
+    public GameObject panelCarteiraLancar;
+    public GameObject panelRegras;
+    public GameObject panelRegras_Cadastrar;
+    public GameObject panelSemanadas;
+    public GameObject panelConquista;
+    public GameObject panelConquista_editar;
+    public GameObject panelAvaliacao;
 
     public GameObject panelPreGame;
     public Image nuvem;
@@ -28,141 +35,334 @@ public class UIManager : MonoBehaviour
     {
         panelLogin.SetActive(true);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
     public void OpenRegistration()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(true);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
-    public void OpenWallet()
+    public void OpenCarteira()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(true);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(true);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
-    public void OpenEconomy()
+    public void OpenCarteiraLancar()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(true);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(true);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
-    public void OpenFamily()
+    public void OpenBancoDaCasa()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(true);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(true);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenBancoDaCasalancar()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(true);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenRegrasDaCasa()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(true);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenRegrasDaCasalancar()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(true);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenSemanadas()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(true);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenConquista()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(true);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenConquistaEditar()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(true);
+        panelAvaliacao.SetActive(false);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
+    }
+    public void OpenAvaliacao()
+    {
+        panelLogin.SetActive(false);
+        panelRegistration.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(true);
+        panelQuizz.SetActive(false);
+        panelLevelSelector.SetActive(false);
+        panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
     public void OpenSettings()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(true);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(true);
     }
 
     public void OpenRules()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(true);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
     public void OpenQuizz()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(true);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
     public void OpenLevelSelector()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(true);
         panelPreQuizz.SetActive(false);
         panelPreGame.SetActive(false);
+        panelSettings.SetActive(false);
     }
 
     public void OpenPreQuizz()
     {
         panelLogin.SetActive(false);
         panelRegistration.SetActive(false);
-        panelWallet.SetActive(false);
-        panelEconomy.SetActive(false);
-        panelFamily.SetActive(false);
-        panelSettings.SetActive(false);
-        panelRules.SetActive(false);
+        panelBanco_da_Casa.SetActive(false);
+        panelBanco_da_Casa_Lancar.SetActive(false);
+        panelCarteira.SetActive(false);
+        panelCarteiraLancar.SetActive(false);
+        panelRegras.SetActive(false);
+        panelRegras_Cadastrar.SetActive(false);
+        panelSemanadas.SetActive(false);
+        panelConquista.SetActive(false);
+        panelConquista_editar.SetActive(false);
+        panelAvaliacao.SetActive(false);
         panelQuizz.SetActive(false);
         panelLevelSelector.SetActive(false);
         panelPreQuizz.SetActive(true);
+        panelSettings.SetActive(false);
     }
     public void OpenPreLevel(int level)
     {
