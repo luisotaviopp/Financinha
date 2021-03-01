afbe63bf758457ce0df9145bd8dfa13af8c43b44
dDaniel <danielpne@hotmail.com>
2021-03-01 01:17:29 -0300

    game play plataforma level 1
    game play plataforma level 1

---
+++ [Unity] Financinha/Assets/Scripts/Gameplay/PlataformBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class PlataformBehavior : ObstacleBehaviour
+    {
+        public GameObject plataforma;
+        public Transform posicao;
+        public bool spawnCan;
+        void Update()
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            
+            if (transform.position.x <= -19.85f)
+            {
+                transform.position = new Vector2(20, transform.position.y);
+            }
+           
+        }
+    }
+}
