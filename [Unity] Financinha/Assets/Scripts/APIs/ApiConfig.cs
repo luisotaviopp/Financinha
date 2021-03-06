f6052a5e5c9e0eefbc5cb370c6f4dabeaa1472e8
Luis Zimermann <luis.zimermann@equilibriumlogistica.com.br>
2021-03-06 17:50:46 -0300

    Scripts para integrar o quiz
    Scripts para integrar o quiz

--- [Unity] Financinha/Assets/Scripts/APIs/ApiConfig.cs
+++ [Unity] Financinha/Assets/Scripts/APIs/ApiConfig.cs
@@ -6,29 +6,29 @@ public class ApiConfig : MonoBehaviour
 
     public static string API_URL = "http://fluxfacility.com/backend-financinha/api/1.0.0/";
 
-    public static string REGISTER_URL = API_URL+ "create_user.php";                 // Cadastrar jogador novo. *****
+    public static string REGISTER_URL = API_URL+ "create_user.php";                             // Cadastrar jogador novo. *****
 
-    public static string LOGIN_URL = API_URL+"login.php";                           // Logar.
+    public static string LOGIN_URL = API_URL+"login.php";                                       // Logar.
 
-    public static string WALLET_AMOUNT_URL = API_URL+"wallet_amount.php";           // Pegar o valor da carteira.
+    public static string WALLET_AMOUNT_URL = API_URL+"wallet_amount.php";                       // Pegar o valor da carteira.
 
-    public static string SAVINGS_AMOUNT_URL = API_URL+"saving_amount.php";          // Pegar o valor do cofrinho.
+    public static string SAVINGS_AMOUNT_URL = API_URL+"saving_amount.php";                      // Pegar o valor do cofrinho.
 
-    public static string GET_LEVEL_URL = API_URL+"user_level.php";                  // Pegar level do jogador.
+    public static string GET_LEVEL_URL = API_URL+"user_level.php";                              // Pegar level do jogador.
 
-    public static string INCREASE_LEVEL_URL = API_URL+"increase_level.php";         // Aumentar level do jogador.
+    public static string INCREASE_LEVEL_URL = API_URL+"increase_level.php";                     // Aumentar level do jogador.
 
-    public static string WALLET_OPERATION_URL = API_URL + "wallet_operation.php";       // Inserindo grana na carteira -> post_value, post_reason
+    public static string WALLET_OPERATION_URL = API_URL + "wallet_operation.php";               // Inserindo grana na carteira -> post_value, post_reason
 
-    public static string WALLET_TO_SAVINGS_URL = API_URL + "wallet_to_savings.php"; // Transferência -> carteira para cofrinho
+    public static string WALLET_TO_SAVINGS_URL = API_URL + "wallet_to_savings.php";             // Transferência -> carteira para cofrinho
 
-    public static string SAVINGS_TO_WALLET_URL = API_URL + "savings_to_wallet.php"; // Transferência -> cofrinho para carteira 
+    public static string SAVINGS_TO_WALLET_URL = API_URL + "savings_to_wallet.php";             // Transferência -> cofrinho para carteira 
 
-    public static string CREATE_RULE_URL = API_URL + "create_rule.php";             // Criando regras da casa
+    public static string CREATE_RULE_URL = API_URL + "create_rule.php";                         // Criando regras da casa
 
-    public static string LIST_RULES_URL = API_URL + "list_rules.php";               // Listando regras da casa
+    public static string LIST_RULES_URL = API_URL + "list_rules.php";                           // Listando regras da casa
 
-    public static string LIST_USERS_URL = API_URL + "search_user.php";              // Procurando um usuário
+    public static string LIST_USERS_URL = API_URL + "search_user.php";                          // Procurando um usuário
 
     // Solicitando relação com outro usuário
 
@@ -40,16 +40,21 @@ public class ApiConfig : MonoBehaviour
 
     // Listar os familiares de um usuário
 
-    public static string EXTRATO_URL = API_URL+"timeline.php";                      // Pegar o extrato do jogador.
+    public static string EXTRATO_URL = API_URL+"timeline.php";                                  // Pegar o extrato do jogador.
 
-    public static string INSERT_OPEN_ANSWER = API_URL + "insert_open_answer.php";   // Cadastrando uma resposta para uma pergunta
+    public static string INSERT_OPEN_ANSWER = API_URL + "insert_open_answer.php";               // Cadastrando uma resposta para uma pergunta
 
-    public static string INSERT_OBJECTIVE = API_URL + "create_objective.php";       // Cadastrar objetivo
+    public static string INSERT_OBJECTIVE = API_URL + "create_objective.php";                   // Cadastrar objetivo
 
-    public static string GET_GIFT_URL = API_URL + "get_objective.php";                   // Pegar objetivo
+    public static string GET_GIFT_URL = API_URL + "get_objective.php";                          // Pegar objetivo
 
-    public static string GET_CAUSE_URL = API_URL + "get_cause.php";                 // Pegar causa
+    public static string GET_CAUSE_URL = API_URL + "get_cause.php";                             // Pegar causa
 
+    public static string GET_QUESTIONS_BY_LEVEL = API_URL + "quiz/get_questions_level.php";     // Pega as questões por level
+
+    public static string INSERT_QUESTION_RESPONSES = API_URL + "quiz/insert_selection.php";     // Envia as respostas pro servidor
+
+    
     // Apenas responsáveis -> Vendo as respostas para as perguntas
 
     // Listar as penalidades de um usuário
---
+++ [Unity] Financinha/Assets/Scripts/APIs/Quiz Answers/GetQuestions.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GetQuestions : MonoBehaviour
+{
+    #region Variaveis
+        public Text questionText;
+        public Text option1Text;
+        public Text option2Text;
+        public Text option3Text;
+        public Text option4Text;
+        public Text option5Text;
+
+        public GameObject levelPanel;
+        public GameObject quizPanel;
+        public GameObject openAnswerPanel;
+
+        private int questionIndex = 0;
+
+        public List<SelectedAnswer> selectedAnswers;
+
+        private QuestionsList questionsList;
+    #endregion
+
+    public void GetInfo(int level)
+    {
+        quizPanel.SetActive(true);
+        levelPanel.SetActive(false);
+        StartCoroutine(GetQuestionsCorroutine(level));
+    }
+
+    IEnumerator GetQuestionsCorroutine(int level)
+    {
+
+        WWWForm form = new WWWForm();
+
+        //form.AddField("post_token", PlayerPrefs.GetString("token"));
+        form.AddField("post_level", level);
+
+        UnityWebRequest www = UnityWebRequest.Post(ApiConfig.GET_QUESTIONS_BY_LEVEL, form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            //Retorna a lista de questões
+            string result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+            questionsList = JsonUtility.FromJson<QuestionsList>("{\"questions\":" + result + "}");
+
+            questionText.text = questionsList.questions[questionIndex].question;
+            option1Text.text = questionsList.questions[questionIndex].option_1;
+            option2Text.text = questionsList.questions[questionIndex].option_2;
+            option3Text.text = questionsList.questions[questionIndex].option_3;
+            option4Text.text = questionsList.questions[questionIndex].option_4;
+            option5Text.text = questionsList.questions[questionIndex].option_5;
+
+            quizPanel.SetActive(true);
+            levelPanel.SetActive(false);
+        }
+    }
+
+    public void NextQuestion()
+    {
+        if (questionIndex < 4)
+        {
+            questionIndex++;
+
+            questionText.text = questionsList.questions[questionIndex].question;
+            option1Text.text = questionsList.questions[questionIndex].option_1;
+            option2Text.text = questionsList.questions[questionIndex].option_2;
+            option3Text.text = questionsList.questions[questionIndex].option_3;
+            option4Text.text = questionsList.questions[questionIndex].option_4;
+            option5Text.text = questionsList.questions[questionIndex].option_5;
+        }
+    }
+
+    public void PreviousQuestion()
+    {
+        if (questionIndex > 0)
+        {
+            questionIndex--;
+            questionText.text = questionsList.questions[questionIndex].question;
+            option1Text.text = questionsList.questions[questionIndex].option_1;
+            option2Text.text = questionsList.questions[questionIndex].option_2;
+            option3Text.text = questionsList.questions[questionIndex].option_3;
+            option4Text.text = questionsList.questions[questionIndex].option_4;
+            option5Text.text = questionsList.questions[questionIndex].option_5;
+        }
+        else
+        {
+            levelPanel.SetActive(true);
+            quizPanel.SetActive(false);
+        }
+    }
+
+    public void SelectAnswer(int option)
+    {
+        // Verifica se já tem alguma resposta cadastrada para essa questão
+        SelectedAnswer selected = selectedAnswers.Find((x) => x.questionId == questionsList.questions[questionIndex].id);
+
+        if (selected == null)
+        {
+            // Monta o objeto a ser salvo.
+            SelectedAnswer newSelection = new SelectedAnswer
+            {
+                questionId = questionsList.questions[questionIndex].id,
+                selectedAnswer = option
+            };
+
+            // Salva a resposta
+            selectedAnswers.Add(newSelection);
+
+            Debug.Log("Cadastrando nova resposta");
+        }
+        else
+        {
+            // Pega o index onde está a resposta cadastrada.
+            int index = selectedAnswers.IndexOf(selected);
+
+            // Remove no index da resposta selecionada anteriormente.
+            selectedAnswers.RemoveAt(index);
+
+            //Adiciona mais um registro na lista de respostas selecionadas.
+            SelectedAnswer newSelection = new SelectedAnswer()
+            {
+                questionId = questionsList.questions[questionIndex].id,
+                selectedAnswer = option
+            };
+
+            // Salva a resposta nova
+            selectedAnswers.Add(newSelection);
+
+            Debug.Log("Substituindo a resposta já selecionada.");
+        }
+    }
+
+    public void SendAnswersToServer()
+    {
+        selectedAnswers.ForEach((x) => {
+            StartCoroutine(SendSelectedAnswers(x));
+        });
+
+        levelPanel.SetActive(true);
+        quizPanel.SetActive(false);
+
+        selectedAnswers.Clear();
+    }
+
+    IEnumerator SendSelectedAnswers(SelectedAnswer answer)
+    {
+        WWWForm form = new WWWForm();
+
+        //form.AddField("post_token", PlayerPrefs.GetString("token"));
+        form.AddField("post_question_id", answer.questionId);
+        form.AddField("post_selected_option", answer.selectedAnswer);
+
+        UnityWebRequest www = UnityWebRequest.Post(ApiConfig.INSERT_QUESTION_RESPONSES, form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+}
